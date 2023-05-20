ccf900ad02a4c22c8025cfd0a72f3016617a0626
ASHISH KUMAR SHARMA <ashishsharma.mail10@gmail.com>
2023-05-20 23:53:28 +0530

    Adding Modes

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const switch1=document.getElementById("switch1")
+
+switch1.onclick=()=>{
+  document.body.classList.toggle("light-theme");
+  if(document.body.classList.contains("light-theme")){
+    
+  }
+}
+
 var typed = new Typed(".span1", {
   strings: ["Student", "Web Developer"],
   typeSpeed: 140,
