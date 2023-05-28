6b12f183898973366770f6c9740620e6d0d69079
ASHISH KUMAR SHARMA <ashishsharma.mail10@gmail.com>
2023-05-28 11:19:58 +0530

    Added Mode

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const switch1=document.getElementById("switch1")
-
-switch1.onclick=()=>{
+const switch1 = document.getElementById("switch1");
+const modeicon = document.getElementById("mode-icon");
+switch1.onclick = (icon) => {
   document.body.classList.toggle("light-theme");
-  if(document.body.classList.contains("light-theme")){
-    
+  if (document.body.classList.contains("light-theme")) {
+    modeicon.src = "assets/half-moon.png";
+  } else {
+    modeicon.src = "assets/light.png";
   }
-}
+};
 
 var typed = new Typed(".span1", {
   strings: ["Student", "Web Developer"],
