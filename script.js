310fe343487467e0a47ea0cfbabdcb5030124c88
ASHISH KUMAR SHARMA <ashishsharma.mail10@gmail.com>
2023-06-06 23:23:29 +0530

    Added Default Mode

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const switch1 = document.getElementById("switch1");
 const modeicon = document.getElementById("mode-icon");
 switch1.onclick = (icon) => {
-  document.body.classList.toggle("light-theme");
-  if (document.body.classList.contains("light-theme")) {
-    modeicon.src = "assets/half-moon.png";
-  } else {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
     modeicon.src = "assets/light.png";
+  } else {
+    modeicon.src = "assets/half-moon.png";
   }
 };
 
